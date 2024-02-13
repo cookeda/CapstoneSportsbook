d68d5047056b1dd0dedce805a48a764b9a99df1a
lknconnor <rkdconnor@gmail.com>
2024-02-13 15:58:38 -0500

    New ideas for Alg

--- Data Collecting Src/alg.py
+++ Data Collecting Src/alg.py
@@ -2,7 +2,7 @@
 # Should be updated to reflect per bet and compare if a bet is good or bad
 # Should take into consideration home advantage, etc. down the line
 # Pretty sure the math is right currently
-def alg(stat):
+def generalAlg(stat):
     type2 = ""
     if stat == "Cover":
         type2 = stat
@@ -36,18 +36,28 @@ def bestOdds(result_dict, stat):
             bestChance = percent
             bestTeam = team
     if stat == "Cover":
-        print("The Most Likely Team to Cover is: " + bestTeam)
+        print("Generally, the most likely team to cover is: " + bestTeam + " Chance: " + str(bestChance))
 
     if stat == "Over":
-        print("The Most Likely Team to go Over is: " + bestTeam)
+        print("Generally, the most likely team to go over is: " + bestTeam + " Chance: " + str(bestChance))
 
 
 def main():
-    dict1 = alg("Cover")
-    dict2 = alg("Over")
-    bestOdds(dict1, "Cover")
-    bestOdds(dict2, "Over")
+    # Very basic prediction calculating on account for
+    # current season, last 10 years, and all time
+    generalCover = generalAlg("Cover")
+    generalOver = generalAlg("Over")
+    bestOdds(generalCover, "Cover")
+    bestOdds(generalOver, "Over")
+
 
 
 if __name__ == '__main__':
     main()
+
+# Idea for new Algorithm:
+# Pass 3 (or more) parameters through that you want to use to determine the algorithm
+# For Example
+# alg(Team, Location, Type, Stat)         alg(Magic, Home, Cover, 2.5)
+# Calculates how well the Magic do at home against the spread
+# Compares that value to the value of 2.5, returns how likely it is to hit
