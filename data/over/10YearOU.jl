5ce8d3fc6cd835164a652a4d8bc0fd3fbd3cdba3
lknconnor <rkdconnor@gmail.com>
2024-02-13 15:07:05 -0500

    Test

---
+++ data/over/10YearOU.jl
@@ -0,0 +1,15 @@
+{"Team": "Washington", "Over %": "53.6%"}
+{"Team": "Minnesota", "Over %": "53.1%"}
+{"Team": "Toronto", "Over %": "52.4%"}
+{"Team": "San Antonio", "Over %": "52.1%"}
+{"Team": "Milwaukee", "Over %": "51.7%"}
+{"Team": "Portland", "Over %": "51.7%"}
+{"Team": "Denver", "Over %": "51.6%"}
+{"Team": "Detroit", "Over %": "51.5%"}
+{"Team": "New Orleans", "Over %": "51.1%"}
+{"Team": "Phoenix", "Over %": "50.7%"}
+{"Team": "Atlanta", "Over %": "50.5%"}
+{"Team": "Philadelphia", "Over %": "50.4%"}
+{"Team": "Boston", "Over %": "50.3%"}
+{"Team": "Sacramento", "Over %": "50.3%"}
+{"Team": "Indiana", "Over %": "50.0%"}
---
+++ data/over/CurrentSeasonOU.jl
@@ -0,0 +1,30 @@
+{"Team": "Philadelphia", "Over %": "60.4%"}
+{"Team": "Detroit", "Over %": "58.8%"}
+{"Team": "Okla City", "Over %": "57.7%"}
+{"Team": "Atlanta", "Over %": "57.4%"}
+{"Team": "Chicago", "Over %": "56.6%"}
+{"Team": "Utah", "Over %": "56.6%"}
+{"Team": "Indiana", "Over %": "55.6%"}
+{"Team": "Golden State", "Over %": "54.9%"}
+{"Team": "LA Lakers", "Over %": "54.6%"}
+{"Team": "Milwaukee", "Over %": "53.7%"}
+{"Team": "Sacramento", "Over %": "53.1%"}
+{"Team": "Charlotte", "Over %": "52.8%"}
+{"Team": "Toronto", "Over %": "51.9%"}
+{"Team": "San Antonio", "Over %": "50.9%"}
+{"Team": "Dallas", "Over %": "50.9%"}
+{"Team": "Phoenix", "Over %": "50.0%"}
+{"Team": "Boston", "Over %": "49.1%"}
+{"Team": "Orlando", "Over %": "48.1%"}
+{"Team": "Minnesota", "Over %": "48.1%"}
+{"Team": "New Orleans", "Over %": "47.2%"}
+{"Team": "Brooklyn", "Over %": "47.1%"}
+{"Team": "Washington", "Over %": "46.2%"}
+{"Team": "Houston", "Over %": "46.2%"}
+{"Team": "Cleveland", "Over %": "45.1%"}
+{"Team": "Portland", "Over %": "44.2%"}
+{"Team": "Memphis", "Over %": "42.6%"}
+{"Team": "LA Clippers", "Over %": "41.2%"}
+{"Team": "New York", "Over %": "40.7%"}
+{"Team": "Miami", "Over %": "39.6%"}
+{"Team": "Denver", "Over %": "37.0%"}
