50655b842c4dc551b96ffda7268a0747cc1e1377
cookeda <cookeda@appstate.edu>
2024-03-04 18:15:52 -0500

    test push

---
+++ data/NBA/cover/10YearCover.jl
@@ -0,0 +1,7 @@
+{"Team": "Boston", "Cover %": "54.0%"} +0.9
+{"Team": "Okla City", "Cover %": "52.4%"} +0.2
+{"Team": "Philadelphia", "Cover %": "51.5%"} +0.3
+{"Team": "LA Clippers", "Cover %": "51.4%"} +0.4
+{"Team": "Miami", "Cover %": "51.3%"} +0.0
+{"Team": "Dallas", "Cover %": "51.1%"} +0.3
+{"Team": "Golden State", "Cover %": "51.0%"} +0.0
