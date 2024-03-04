a4c37cbf197807ea4d364d1bebd9a9fcb7059fdb
lknconnor <rkdconnor@gmail.com>
2024-03-04 18:16:47 -0500

    stuff

---
+++ data/NBA/cover/CurrentSeasonCover.jl
@@ -0,0 +1,10 @@
+{"Team": "Orlando", "Cover %": "65.6%"} +2.0
+{"Team": "Okla City", "Cover %": "64.4%"} +4.1
+{"Team": "Golden State", "Cover %": "55.9%"} -0.6
+{"Team": "Philadelphia", "Cover %": "55.0%"} +0.4
+{"Team": "New Orleans", "Cover %": "55.0%"} +2.6
+{"Team": "Utah", "Cover %": "53.3%"} -0.3
+{"Team": "Indiana", "Cover %": "53.3%"} +0.9
+{"Team": "Boston", "Cover %": "52.6%"} +2.9
+{"Team": "New York", "Cover %": "52.5%"} +2.0
+{"Team": "LA Clippers", "Cover %": "52.5%"} +0.1
