e8627e9efe57fc6e4053738d2bb678a6a4fa46a4
cookeda <cookeda@appstate.edu>
2024-03-19 10:20:52 -0400

    Script Works (WIP)

---
+++ Data Collecting Src/script.py
@@ -0,0 +1,12 @@
+import subprocess
+
+process1 = subprocess.Popen(["python", "CBBScrape.py"]) # Create and launch process pop.py using python interpreter
+process2 = subprocess.Popen(["python", "MLBScrape.py"])
+process3 = subprocess.Popen(["python", "NBAScrape.py"])
+
+process1.wait() # Wait for process1 to finish (basically wait for script to finish)
+process2.wait()
+process3.wait()
+
+#process4 = subprocess.Popen(["python", "Sort.py"])
+#process5 = subprocess.Popen(["python", "alg.py"])
