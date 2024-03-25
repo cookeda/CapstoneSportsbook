6ac71316ca1990dc6362f135ddbbc9cd8a6f5ce9
lknconnor <rkdconnor@gmail.com>
2024-03-25 12:28:58 -0400

    Added MOV to Alg

--- Data Collecting Src/alg.py
+++ Data Collecting Src/alg.py
@@ -89,7 +89,7 @@ def bestOddsGeneral(result_dict, stat):
         print("Generally, the most likely team to go over is: " + bestTeam + " Chance: " + str(bestChance))
 
 # Gets python dictionary from a file
-def getDict(file):
+def getDictPercent(file):
     result_dict = {}
     with open(file, 'r') as fr:
         for line in fr:
@@ -100,6 +100,21 @@ def getDict(file):
             percent = float(percent_search.group()) / 100 if percent_search else 0.0
 
             result_dict[team] = percent
+    return result_dict
+
+def getDictMOV(file):
+    result_dict = {}
+    with open(file, 'r') as fr:
+        for line in fr:
+            # First, extract the team name from the JSON-like structure
+            team_search = re.search(r'"Team":\s*"([^"]+)"', line)
+            team = team_search.group(1) if team_search else "Unknown"
+
+            # Now, extract the MOV value from the end of the line
+            mov_search = re.search(r'([-]?\d+\.\d+)$', line)
+            mov = float(mov_search.group(1)) if mov_search else 0.0
+
+            result_dict[team] = mov
     return result_dict
 
 def combineOnRanking(dict1, dict2):
@@ -118,7 +133,7 @@ def sortByRank(input_dict):
     ranked_dict = {team: rank for rank, (team, percent) in enumerate(sorted_dict, start=1)}
     return ranked_dict
 
-def gameInput(homeTeam, awayTeam, league):
+def gameInput(homeTeam, homeSpread, awayTeam, awaySpread, league):
     print("-----------------------------")
     try:
         with open(direct + 'results.txt', 'a') as fp:
@@ -133,6 +148,8 @@ def gameInput(homeTeam, awayTeam, league):
         coverAway = cacNBA[awayTeam]
         overHome = chcNBA[homeTeam]
         overAway = caoNBA[awayTeam]
+        movHome = homeMOV[homeTeam]
+        movAway = awayMOV[awayTeam]
 
     elif league == 'CBB':
         numTeams = len(choCBB)
@@ -140,6 +157,8 @@ def gameInput(homeTeam, awayTeam, league):
         coverAway = cacCBB[awayTeam]
         overHome = chcCBB[homeTeam]
         overAway = caoCBB[awayTeam]
+        movHome = homeMOV2[homeTeam]
+        movAway = awayMOV2[awayTeam]
 
     topTier = numTeams * 0.10  # Top 10% Change to 20
     midTier = numTeams * 0.322  # Top 30% Change to 30
@@ -203,7 +222,9 @@ def gameInput(homeTeam, awayTeam, league):
             print(f"Error writing to file: {e}")
 
     # Cover
-    if coverHome <= midTier and coverAway > lowTier:
+    print("HOME: " + "MOV: " + str(movHome) + " Spread: " + str(homeSpread))
+    print("AWAY: " + "MOV: " + str(movAway) + " Spread: " + str(awaySpread))
+    if coverHome <= midTier and coverAway > lowTier or ((float(movHome) + float(homeSpread)) > 0):
         if coverHome <= topTier and coverAway > ass:
             print("LOCK ALERT!")
             try:
@@ -221,7 +242,7 @@ def gameInput(homeTeam, awayTeam, league):
         print("Home Rank :" + str(coverHome))
         print("Away Rank: " + str(coverAway))
         parlay.append(league + ": " + homeTeam + ": Cover")
-    elif coverHome > lowTier and coverAway <= midTier:
+    elif coverHome > lowTier and coverAway <= midTier or ((float(movAway) + float(awaySpread)) > 0):
         if coverHome > ass and coverAway <= topTier:
             print("LOCK ALERT!")
             try:
@@ -275,7 +296,12 @@ def gameInputFromJSON(file, league):
         awayTeam = game["Away Team Rank Name"]
         homeSpread = game['DK Home Odds']['Spread']
         awaySpread = game['DK Away Odds']['Spread']
-        gameInput(homeTeam, awayTeam, league)
+        if homeSpread == "Pick)" or homeSpread == "-Pick)":
+            homeSpread = "0"
+
+        if awaySpread == "Pick" or awaySpread == "-Pick":
+            awaySpread = "0"
+        gameInput(homeTeam, homeSpread, awayTeam, awaySpread, league)
 
 def cleanfile(file):
     try:
@@ -296,12 +322,18 @@ def main():
     global homeCover    # Home Cover Dict
     global homeCover2
     global homeCover3
+    global homeMOV     # Home Margin of Victory
+    global homeMOV2
+    global homeMOV3
     global awayOver     # Away Over Dict
     global awayOver2
     global awayOver3
     global awayCover    # Away Cover Dict
     global awayCover2
     global awayCover3
+    global awayMOV     # Away Margin of Victory
+    global awayMOV2
+    global awayMOV3
     global choNBA          # Combined Home Over
     global choCBB
     global choMLB
@@ -336,22 +368,30 @@ def main():
     generalOver3 = generalAlg("Over", "MLB")
 
     # For Home
-    homeCover = getDict(direct + "NBA/cover/home/SortedhomeCover.jl")
-    homeCover2 = getDict(direct + "CBB/cover/home/SortedhomeCover.jl")
+    homeCover = getDictPercent(direct + "NBA/cover/home/SortedhomeCover.jl")
+    homeCover2 = getDictPercent(direct + "CBB/cover/home/SortedhomeCover.jl")
     #homeCover3 = getDict(direct + "MLB/cover/home/SortedhomeCover.jl")
 
-    homeOver = getDict(direct + "NBA/over/home/SortedhomeOver.jl")
-    homeOver2 = getDict(direct + "CBB/over/home/SortedhomeOver.jl")
+    homeOver = getDictPercent(direct + "NBA/over/home/SortedhomeOver.jl")
+    homeOver2 = getDictPercent(direct + "CBB/over/home/SortedhomeOver.jl")
     #homeOver3 = getDict(direct + "MLB/over/home/SortedHomeOver.jl")
 
+    homeMOV = getDictMOV("../data/NBA/cover/home/SortedhomeCover.jl")
+    homeMOV2 = getDictMOV("../data/CBB/cover/home/SortedhomeCover.jl")
+    #homeMOV3 = getDictMOV("../data/MLB/cover/home/SortedhomeCover.jl")
+
     # For Away
-    awayCover = getDict(direct + "NBA/cover/away/SortedawayCover.jl")
-    awayCover2 = getDict(direct + "CBB/cover/away/SortedawayCover.jl")
+    awayCover = getDictPercent(direct + "NBA/cover/away/SortedawayCover.jl")
+    awayCover2 = getDictPercent(direct + "CBB/cover/away/SortedawayCover.jl")
     #awayCover3 = getDict(direct + "MLB/cover/away/SortedawayCover.jl")
 
-    awayOver = getDict(direct + "NBA/over/away/SortedawayOver.jl")
-    awayOver2 = getDict(direct + "CBB/over/away/SortedawayOver.jl")
+    awayOver = getDictPercent(direct + "NBA/over/away/SortedawayOver.jl")
+    awayOver2 = getDictPercent(direct + "CBB/over/away/SortedawayOver.jl")
     #awayOver3 = getDict(direct + "MLB/over/away/SortedawayOver.jl")
+
+    awayMOV = getDictMOV("../data/NBA/cover/away/SortedawayCover.jl")
+    awayMOV2 = getDictMOV("../data/CBB/cover/away/SortedawayCover.jl")
+    #awayMOV3 = getDictMOV("../data/MLB/cover/away/SortedawayCover.jl")
 
     # Variables stand for Combine Home/Away Over/Cover (ex: Combine Home Over = cho)
     choNBA = combineOnRanking(sortByRank(generalOver), sortByRank(homeOver))
