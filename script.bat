c8ed3169d5201acbb2a84d5d79b94c2add1d002c
cookeda <cookeda@appstate.edu>
2024-03-27 10:25:51 -0400

    documented batch file, added time stamps

--- script.bat
+++ script.bat
@@ -1,11 +1,25 @@
+@echo off
+:: Capture start time
+set start_time=%time%
+
+:: Scrape Adv League Data
 cd OddsHistory
 scrapy crawl NBA -o NBA.json
+:: Scrape Matchups
 cd ../
 cd Scrapers/Books
 python book_scripts.py
+:: Scrape League Data
 cd ../../Data Collecting Src/
 python MLBScrape.py
 python NBAScrape.py
 python CBBScrape.py
 python Sort.py
 python alg.py
+
+:: Capture end time
+set end_time=%time%
+
+:: Display start and end times
+echo Start Time: %start_time%
+echo End Time: %end_time%
