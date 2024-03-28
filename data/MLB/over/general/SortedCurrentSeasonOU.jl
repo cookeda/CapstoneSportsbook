d5e196e5a2a1f29eb213e45c8e685dd203a30047
lknconnor <rkdconnor@gmail.com>
2024-03-28 14:51:32 -0400

    This prolly made everything worse but

--- data/MLB/over/general/SortedCurrentSeasonOU.jl
+++ data/MLB/over/general/SortedCurrentSeasonOU.jl
@@ -1,2 +1,2 @@
-{"Team": "LA Dodgers", "Over %": "50.0%"} -2.0
-{"Team": "San Diego", "Over %": "50.0%"} +2.0
+{"Team": "LA Dodgers", "Over %": "50.0%"} +7.8
+{"Team": "San Diego", "Over %": "50.0%"} +7.8
