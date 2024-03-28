2393547f7d7ebc2aca62ecc2c5cfab74ae0659fd
lknconnor <rkdconnor@gmail.com>
2024-03-28 14:51:32 -0400

    This prolly made everything worse but

--- data/MLB/cover/general/Sorted10YearCover.jl
+++ data/MLB/cover/general/Sorted10YearCover.jl
@@ -1,2 +1,30 @@
-{"Team": "LA Dodgers", "Cover %": "50.0%"} -2.0
-{"Team": "San Diego", "Cover %": "50.0%"} +2.0
+{"Team": "Arizona", "Cover %": "48.8%"} -0.2
+{"Team": "Atlanta", "Cover %": "46.9%"} -0.7
+{"Team": "Baltimore", "Cover %": "57.4%"} +0.7
+{"Team": "Boston", "Cover %": "42.6%"} -0.5
+{"Team": "Chi Cubs", "Cover %": "51.2%"} +0.3
+{"Team": "Chi Sox", "Cover %": "50.0%"} +0.2
+{"Team": "Cincinnati", "Cover %": "51.9%"} +0.1
+{"Team": "Cleveland", "Cover %": "48.8%"} -0.2
+{"Team": "Colorado", "Cover %": "50.6%"} +0.2
+{"Team": "Detroit", "Cover %": "47.9%"} -0.6
+{"Team": "Houston", "Cover %": "56.2%"} +0.5
+{"Team": "Kansas City", "Cover %": "55.4%"} +0.0
+{"Team": "LA Angels", "Cover %": "52.1%"} +0.2
+{"Team": "LA Dodgers", "Cover %": "50.0%"} -0.3
+{"Team": "Miami", "Cover %": "51.9%"} +0.5
+{"Team": "Milwaukee", "Cover %": "46.9%"} -0.4
+{"Team": "Minnesota", "Cover %": "49.4%"} +0.6
+{"Team": "NY Mets", "Cover %": "56.8%"} +0.8
+{"Team": "NY Yankees", "Cover %": "50.0%"} -0.2
+{"Team": "Oakland", "Cover %": "47.2%"} -0.1
+{"Team": "Philadelphia", "Cover %": "50.0%"} +0.2
+{"Team": "Pittsburgh", "Cover %": "52.8%"} -0.1
+{"Team": "SF Giants", "Cover %": "52.5%"} +0.0
+{"Team": "San Diego", "Cover %": "46.3%"} +0.1
+{"Team": "Seattle", "Cover %": "56.2%"} +0.3
+{"Team": "St. Louis", "Cover %": "42.7%"} -0.6
+{"Team": "Tampa Bay", "Cover %": "42.0%"} -0.6
+{"Team": "Texas", "Cover %": "43.8%"} -0.3
+{"Team": "Toronto", "Cover %": "53.7%"} +0.3
+{"Team": "Washington", "Cover %": "47.6%"} -0.1
