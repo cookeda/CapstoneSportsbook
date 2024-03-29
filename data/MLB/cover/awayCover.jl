a46cbaab892a54b782e3949e1b664b584e17bb63
cookeda <cookeda@appstate.edu>
2024-03-29 11:27:18 -0400

    3/29

--- data/MLB/cover/awayCover.jl
+++ data/MLB/cover/awayCover.jl
@@ -1,13 +1,13 @@
+{"Team": "Cleveland", "Cover %": "100.0%"} +6.5
+{"Team": "Minnesota", "Cover %": "100.0%"} +1.5
+{"Team": "Pittsburgh", "Cover %": "100.0%"} +2.5
+{"Team": "Chi Cubs", "Cover %": "100.0%"} +0.5
+{"Team": "NY Yankees", "Cover %": "100.0%"} +2.5
 {"Team": "Boston", "Cover %": "100.0%"} +3.5
 {"Team": "Toronto", "Cover %": "100.0%"} +7.5
-{"Team": "NY Yankees", "Cover %": "100.0%"} +2.5
-{"Team": "Cleveland", "Cover %": "100.0%"} +6.5
-{"Team": "Chi Cubs", "Cover %": "100.0%"} +0.5
-{"Team": "Pittsburgh", "Cover %": "100.0%"} +2.5
-{"Team": "Minnesota", "Cover %": "100.0%"} +1.5
+{"Team": "St. Louis", "Cover %": "0.0%"} -4.5
+{"Team": "Colorado", "Cover %": "0.0%"} -13.5
+{"Team": "SF Giants", "Cover %": "0.0%"} -0.5
+{"Team": "Washington", "Cover %": "0.0%"} -4.5
 {"Team": "LA Angels", "Cover %": "0.0%"} -6.5
 {"Team": "Detroit", "Cover %": "0.0%"} -0.5
-{"Team": "SF Giants", "Cover %": "0.0%"} -0.5
-{"Team": "Colorado", "Cover %": "0.0%"} -13.5
-{"Team": "Washington", "Cover %": "0.0%"} -4.5
-{"Team": "St. Louis", "Cover %": "0.0%"} -4.5
--- data/MLB/over/homeOver.jl
+++ data/MLB/over/homeOver.jl
@@ -3,11 +3,11 @@
 {"Team": "Tampa Bay", "Over %": "100.0%"} +2.5
 {"Team": "Cincinnati", "Over %": "100.0%"} +0.5
 {"Team": "Arizona", "Over %": "100.0%"} +9.0
+{"Team": "San Diego", "Over %": "100.0%"} +2.5
 {"Team": "Miami", "Over %": "100.0%"} +3.5
 {"Team": "Seattle", "Over %": "100.0%"} +2.5
-{"Team": "San Diego", "Over %": "100.0%"} +2.5
-{"Team": "Chi Sox", "Over %": "0.0%"} -6.5
-{"Team": "Kansas City", "Over %": "0.0%"} -3.5
-{"Team": "LA Dodgers", "Over %": "0.0%"} -1.0
-{"Team": "Houston", "Over %": "0.0%"} +0.0
 {"Team": "Texas", "Over %": "0.0%"} -1.5
+{"Team": "Houston", "Over %": "0.0%"} +0.0
+{"Team": "Kansas City", "Over %": "0.0%"} -3.5
+{"Team": "Chi Sox", "Over %": "0.0%"} -6.5
+{"Team": "LA Dodgers", "Over %": "0.0%"} -1.0
