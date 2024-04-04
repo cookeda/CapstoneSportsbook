47e964779b555b7d20450982184de5e7ca34282c
cookeda <cookeda@appstate.edu>
2024-04-04 13:28:56 -0400

    Expo Setup Done

--- DegenBets/App.js
+++ DegenBets/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! VsCodeUpdate</Text>
+      <Text>This should be pushed to git and expo repos at the same time</Text>
       <StatusBar style="auto" />
     </View>
   );
