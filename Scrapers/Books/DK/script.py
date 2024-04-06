907743069ed21d2b9efb8fe7d8cb305339cb0858
cookeda <cookeda@appstate.edu>
2024-04-06 14:29:32 -0400

    Batch Files Updated, daily.bat (run first) update.bat(run to refresh odds)

--- Scrapers/Books/DK/script.py
+++ Scrapers/Books/DK/script.py
@@ -6,12 +6,14 @@ process2 = subprocess.Popen(["python", "mlb_dk.py"])
 process3 = subprocess.Popen(["python", "nba_dk.py"])
 
 print("Scraping DraftKings for: CBB, NBA, MLB")
-
-process1.wait() # Wait for process1 to finish (basically wait for script to finish)
+process1.wait()
 process2.wait()
 process3.wait()
 
-#time.sleep(5)
 
-#process4 = subprocess.Popen(["python", "Sort.py"])
-#process5 = subprocess.Popen(["python", "alg.py"])
+# After scraping is complete, process the data with lite_writer.py
+process5 = subprocess.Popen(["python", "../lite_writer.py", "../../Data/DK/NBA.json", "../../Data/DK/NBA_Lite.json"])
+process6 = subprocess.Popen(["python", "../lite_writer.py", "../../Data/DK/CBB.json", "../../Data/DK/CBB_Lite.json"])
+process4 = subprocess.Popen(["python", "../lite_writer.py", "../../Data/DK/MLB.json", "../../Data/DK/MLB_Lite.json"])
+
+print("DK Data processing complete.")
