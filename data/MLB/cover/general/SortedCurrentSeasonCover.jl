fc3af7e83ec419cabcfbbff66a2197be257e021e
lknconnor <rkdconnor@gmail.com>
2024-04-16 11:00:59 -0400

    4/16/24

--- data/MLB/cover/general/SortedCurrentSeasonCover.jl
+++ data/MLB/cover/general/SortedCurrentSeasonCover.jl
@@ -1,30 +1,30 @@
-{"Team": "Arizona", "Cover %": "62.5%"} +0.9
-{"Team": "Atlanta", "Cover %": "42.9%"} -0.1
-{"Team": "Baltimore", "Cover %": "53.3%"} -0.2
-{"Team": "Boston", "Cover %": "56.3%"} +0.9
-{"Team": "Chi Cubs", "Cover %": "66.7%"} +1.6
-{"Team": "Chi Sox", "Cover %": "46.7%"} -1.9
-{"Team": "Cincinnati", "Cover %": "53.3%"} +0.8
-{"Team": "Cleveland", "Cover %": "66.7%"} +1.8
-{"Team": "Colorado", "Cover %": "43.8%"} -0.6
-{"Team": "Detroit", "Cover %": "33.3%"} -0.4
-{"Team": "Houston", "Cover %": "35.3%"} -1.7
-{"Team": "Kansas City", "Cover %": "62.5%"} +2.9
-{"Team": "LA Angels", "Cover %": "46.7%"} +0.3
-{"Team": "LA Dodgers", "Cover %": "44.4%"} -0.8
-{"Team": "Miami", "Cover %": "25.0%"} -2.0
-{"Team": "Milwaukee", "Cover %": "64.3%"} +2.9
-{"Team": "Minnesota", "Cover %": "50.0%"} -0.7
-{"Team": "NY Mets", "Cover %": "46.7%"} -0.2
-{"Team": "NY Yankees", "Cover %": "50.0%"} +0.8
-{"Team": "Oakland", "Cover %": "50.0%"} -0.3
-{"Team": "Philadelphia", "Cover %": "37.5%"} -1.6
-{"Team": "Pittsburgh", "Cover %": "75.0%"} +2.3
-{"Team": "SF Giants", "Cover %": "43.8%"} -0.6
-{"Team": "San Diego", "Cover %": "50.0%"} +0.2
-{"Team": "Seattle", "Cover %": "31.3%"} -1.4
-{"Team": "St. Louis", "Cover %": "62.5%"} +0.0
-{"Team": "Tampa Bay", "Cover %": "43.8%"} -2.0
-{"Team": "Texas", "Cover %": "43.8%"} +0.7
-{"Team": "Toronto", "Cover %": "56.3%"} -0.9
-{"Team": "Washington", "Cover %": "60.0%"} +0.2
+{"Team": "Arizona", "Cover %": "58.8%"} +0.7
+{"Team": "Atlanta", "Cover %": "46.7%"} +0.1
+{"Team": "Baltimore", "Cover %": "56.3%"} -0.1
+{"Team": "Boston", "Cover %": "52.9%"} +0.4
+{"Team": "Chi Cubs", "Cover %": "68.8%"} +1.6
+{"Team": "Chi Sox", "Cover %": "43.8%"} -1.8
+{"Team": "Cincinnati", "Cover %": "50.0%"} +0.4
+{"Team": "Cleveland", "Cover %": "68.8%"} +2.2
+{"Team": "Colorado", "Cover %": "47.1%"} -0.6
+{"Team": "Detroit", "Cover %": "31.3%"} -0.6
+{"Team": "Houston", "Cover %": "33.3%"} -1.8
+{"Team": "Kansas City", "Cover %": "64.7%"} +2.7
+{"Team": "LA Angels", "Cover %": "50.0%"} +0.6
+{"Team": "LA Dodgers", "Cover %": "42.1%"} -1.0
+{"Team": "Miami", "Cover %": "29.4%"} -1.9
+{"Team": "Milwaukee", "Cover %": "60.0%"} +2.3
+{"Team": "Minnesota", "Cover %": "46.7%"} -0.8
+{"Team": "NY Mets", "Cover %": "50.0%"} -0.1
+{"Team": "NY Yankees", "Cover %": "47.1%"} +0.7
+{"Team": "Oakland", "Cover %": "47.1%"} -0.3
+{"Team": "Philadelphia", "Cover %": "35.3%"} -1.5
+{"Team": "Pittsburgh", "Cover %": "70.6%"} +2.0
+{"Team": "SF Giants", "Cover %": "41.2%"} -0.6
+{"Team": "San Diego", "Cover %": "52.6%"} +0.4
+{"Team": "Seattle", "Cover %": "35.3%"} -1.1
+{"Team": "St. Louis", "Cover %": "64.7%"} +0.0
+{"Team": "Tampa Bay", "Cover %": "41.2%"} -2.2
+{"Team": "Texas", "Cover %": "47.1%"} +0.8
+{"Team": "Toronto", "Cover %": "58.8%"} -0.9
+{"Team": "Washington", "Cover %": "62.5%"} +0.4
--- data/MLB/over/general/SortedPointAverages.jl
+++ data/MLB/over/general/SortedPointAverages.jl
@@ -1,30 +1,30 @@
-{"Team": "Arizona", "PPG": "5.56", "Last 3": "5.00", "Home": "6.10", "Away": "4.67"}
-{"Team": "Atlanta", "PPG": "6.50", "Last 3": "6.00", "Home": "6.17", "Away": "6.75"}
-{"Team": "Baltimore", "PPG": "5.47", "Last 3": "4.00", "Home": "5.33", "Away": "5.67"}
-{"Team": "Boston", "PPG": "4.50", "Last 3": "4.00", "Home": "3.67", "Away": "5.00"}
-{"Team": "Chi Cubs", "PPG": "5.47", "Last 3": "3.00", "Home": "7.33", "Away": "4.22"}
-{"Team": "Chi Sox", "PPG": "2.27", "Last 3": "1.67", "Home": "2.00", "Away": "2.57"}
-{"Team": "Cincinnati", "PPG": "6.00", "Last 3": "9.00", "Home": "5.44", "Away": "6.83"}
-{"Team": "Cleveland", "PPG": "5.40", "Last 3": "4.00", "Home": "4.67", "Away": "5.89"}
-{"Team": "Colorado", "PPG": "4.31", "Last 3": "5.00", "Home": "5.00", "Away": "3.90"}
-{"Team": "Detroit", "PPG": "3.73", "Last 3": "3.33", "Home": "3.43", "Away": "4.00"}
-{"Team": "Houston", "PPG": "4.71", "Last 3": "8.33", "Home": "5.50", "Away": "3.57"}
-{"Team": "Kansas City", "PPG": "5.31", "Last 3": "4.33", "Home": "6.10", "Away": "4.00"}
-{"Team": "LA Angels", "PPG": "4.47", "Last 3": "4.33", "Home": "3.83", "Away": "4.89"}
-{"Team": "LA Dodgers", "PPG": "5.33", "Last 3": "5.00", "Home": "5.60", "Away": "5.00"}
-{"Team": "Miami", "PPG": "3.75", "Last 3": "4.33", "Home": "3.70", "Away": "3.83"}
-{"Team": "Milwaukee", "PPG": "6.50", "Last 3": "8.67", "Home": "5.40", "Away": "7.11"}
-{"Team": "Minnesota", "PPG": "3.50", "Last 3": "6.00", "Home": "2.20", "Away": "4.22"}
-{"Team": "NY Mets", "PPG": "4.60", "Last 3": "5.00", "Home": "3.11", "Away": "6.83"}
-{"Team": "NY Yankees", "PPG": "4.94", "Last 3": "6.00", "Home": "4.83", "Away": "5.00"}
-{"Team": "Oakland", "PPG": "2.94", "Last 3": "3.33", "Home": "2.50", "Away": "3.67"}
-{"Team": "Philadelphia", "PPG": "3.63", "Last 3": "2.67", "Home": "3.80", "Away": "3.33"}
-{"Team": "Pittsburgh", "PPG": "5.44", "Last 3": "5.67", "Home": "4.00", "Away": "6.09"}
-{"Team": "SF Giants", "PPG": "4.31", "Last 3": "5.33", "Home": "2.83", "Away": "5.20"}
-{"Team": "San Diego", "PPG": "5.33", "Last 3": "5.33", "Home": "5.50", "Away": "5.13"}
-{"Team": "Seattle", "PPG": "3.06", "Last 3": "2.33", "Home": "2.40", "Away": "4.17"}
-{"Team": "St. Louis", "PPG": "3.81", "Last 3": "3.67", "Home": "3.83", "Away": "3.80"}
-{"Team": "Tampa Bay", "PPG": "4.25", "Last 3": "4.33", "Home": "3.90", "Away": "4.83"}
-{"Team": "Texas", "PPG": "5.38", "Last 3": "6.33", "Home": "5.20", "Away": "5.67"}
-{"Team": "Toronto", "PPG": "3.81", "Last 3": "4.67", "Home": "4.17", "Away": "3.60"}
-{"Team": "Washington", "PPG": "3.73", "Last 3": "3.33", "Home": "3.00", "Away": "4.22"}
+{"Team": "Arizona", "PPG": "5.35", "Last 3": "3.67", "Home": "5.73", "Away": "4.67"}
+{"Team": "Atlanta", "PPG": "6.47", "Last 3": "5.33", "Home": "6.17", "Away": "6.67"}
+{"Team": "Baltimore", "PPG": "5.56", "Last 3": "6.00", "Home": "5.50", "Away": "5.67"}
+{"Team": "Boston", "PPG": "4.24", "Last 3": "4.00", "Home": "3.14", "Away": "5.00"}
+{"Team": "Chi Cubs", "PPG": "5.31", "Last 3": "3.33", "Home": "7.33", "Away": "4.10"}
+{"Team": "Chi Sox", "PPG": "2.13", "Last 3": "1.33", "Home": "1.78", "Away": "2.57"}
+{"Team": "Cincinnati", "PPG": "5.81", "Last 3": "6.33", "Home": "5.44", "Away": "6.29"}
+{"Team": "Cleveland", "PPG": "5.44", "Last 3": "5.33", "Home": "4.67", "Away": "5.90"}
+{"Team": "Colorado", "PPG": "4.12", "Last 3": "1.33", "Home": "5.00", "Away": "3.64"}
+{"Team": "Detroit", "PPG": "3.50", "Last 3": "1.67", "Home": "3.00", "Away": "4.00"}
+{"Team": "Houston", "PPG": "4.50", "Last 3": "6.00", "Home": "5.09", "Away": "3.57"}
+{"Team": "Kansas City", "PPG": "5.12", "Last 3": "4.67", "Home": "6.10", "Away": "3.71"}
+{"Team": "LA Angels", "PPG": "4.63", "Last 3": "4.33", "Home": "3.83", "Away": "5.10"}
+{"Team": "LA Dodgers", "PPG": "5.26", "Last 3": "4.00", "Home": "5.45", "Away": "5.00"}
+{"Team": "Miami", "PPG": "3.71", "Last 3": "5.00", "Home": "3.64", "Away": "3.83"}
+{"Team": "Milwaukee", "PPG": "6.27", "Last 3": "6.00", "Home": "5.00", "Away": "7.11"}
+{"Team": "Minnesota", "PPG": "3.53", "Last 3": "3.67", "Home": "2.20", "Away": "4.20"}
+{"Team": "NY Mets", "PPG": "4.69", "Last 3": "5.00", "Home": "3.40", "Away": "6.83"}
+{"Team": "NY Yankees", "PPG": "4.71", "Last 3": "5.33", "Home": "4.83", "Away": "4.64"}
+{"Team": "Oakland", "PPG": "2.82", "Last 3": "3.00", "Home": "2.36", "Away": "3.67"}
+{"Team": "Philadelphia", "PPG": "3.53", "Last 3": "2.67", "Home": "3.64", "Away": "3.33"}
+{"Team": "Pittsburgh", "PPG": "5.29", "Last 3": "5.00", "Home": "4.00", "Away": "5.83"}
+{"Team": "SF Giants", "PPG": "4.29", "Last 3": "6.33", "Home": "2.83", "Away": "5.09"}
+{"Team": "San Diego", "PPG": "5.42", "Last 3": "5.00", "Home": "5.50", "Away": "5.33"}
+{"Team": "Seattle", "PPG": "3.41", "Last 3": "4.00", "Home": "3.00", "Away": "4.17"}
+{"Team": "St. Louis", "PPG": "3.76", "Last 3": "1.67", "Home": "3.83", "Away": "3.73"}
+{"Team": "Tampa Bay", "PPG": "4.18", "Last 3": "4.67", "Home": "3.82", "Away": "4.83"}
+{"Team": "Texas", "PPG": "5.12", "Last 3": "2.67", "Home": "5.20", "Away": "5.00"}
+{"Team": "Toronto", "PPG": "3.76", "Last 3": "4.33", "Home": "4.00", "Away": "3.60"}
+{"Team": "Washington", "PPG": "3.88", "Last 3": "5.00", "Home": "3.00", "Away": "4.40"}
