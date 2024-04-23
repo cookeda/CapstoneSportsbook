6e284a5b55b20a0ed489023c1188e8385107bea4
lknconnor <rkdconnor@gmail.com>
2024-04-22 21:05:56 -0400

    Final Push For Server

--- Daily.py
+++ Daily.py
@@ -41,6 +41,8 @@ def main():
         subprocess.run(['python3.10', 'alg.py'], stdout=f)
     with open("../Results/game_results.txt", "w") as f:
         subprocess.run(['python3.10', 'game.py'], stdout=f)
+    with open("../Results/SuccessData_results.txt", "w") as f:
+        subprocess.run(['python3.10', 'SuccessData.py'], stdout=f)
     os.chdir("../Results")
     subprocess.run(['python3.10', 'summary.py'])
 
