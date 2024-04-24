92308560a0b818bc4ad3c60df27c77a542c030f6
cookeda <cookeda@appstate.edu>
2024-04-24 16:01:55 -0400

    Bovada Updates, Live almost implemented

--- Scrapers/Books/constant_refresh.py
+++ Scrapers/Books/constant_refresh.py
@@ -2,12 +2,13 @@ import subprocess
 import time
 
 process1 = subprocess.Popen(["python", "script.py"], cwd='DK') # Create and launch process pop.py using python interpreter
-process2 = subprocess.Popen(["python", "script.py"], cwd='ESPN') # Create and launch
-process3 = subprocess.Popen(["python", "script.py"], cwd = 'Bovada')
-
 process1.wait() # Wait for process1 to finish (basically wait for script to finish)
-#process2.wait() # Wait for process2 to finish (basically wait for script to finish)
-#process3.wait()
+time.sleep(5)
+process2 = subprocess.Popen(["python", "script.py"], cwd='ESPN') # Create and launch
+process3 = subprocess.Popen(["python", "script.py"], cwd='Bovada')
+
+process2.wait() # Wait for process2 to finish (basically wait for script to finish)
+process3.wait()
 
 process4 = subprocess.Popen(["python", "master_lite.py"])
 
--- Update.py
+++ Update.py
@@ -21,11 +21,11 @@ subprocess.run(['python', 'summary.py'])
 subprocess.run(['python', 'merger.py'])
 # Uncomment to enable sorting matchup page (Live games not implemeneted yet)
 # Data Processing and Updates
-# os.chdir('../Scrapers/Data Processing')
-# subprocess.run(['python', 'script.py'])
-# os.chdir('../../DegenBets')
+os.chdir('../Scrapers/Data Processing')
+subprocess.run(['python', 'script.py'])
+os.chdir('../../DegenBets')
 # Uncomment the following line to enable the auto update command
-# subprocess.run(['npx', 'eas', 'update', '--auto'])
+#subprocess.run(['npx', 'eas', 'update', '--auto'])
 
 # Capture end time
 end_time = datetime.datetime.now()
