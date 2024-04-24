1eb96366bdd2328c13022296f2a7f3fa521818ee
cookeda <cookeda@appstate.edu>
2024-04-24 12:34:03 -0400

    4/24 Push, final update before games, should be fixed now

--- Daily.py
+++ Daily.py
@@ -7,8 +7,8 @@ def main():
     start_time = datetime.now()
     
     # Run History in Data Collecting Src
-    os.chdir("/media/myfiles/CapstoneSportsbook/Data Collecting Src")
-    subprocess.run(['python3.10', 'history.py'])
+    os.chdir("./Data Collecting Src")
+    subprocess.run(['python', 'history.py'])
 
     # Scrape Adv League Data
     os.chdir("../OddsHistory")
@@ -25,26 +25,26 @@ def main():
 
     # Scrape League Data
     os.chdir("../Data Collecting Src")
-    subprocess.run(['python3.10', 'MLBScrape.py'])
-    subprocess.run(['python3.10', 'NBAScrape.py'])
+    subprocess.run(['python', 'MLBScrape.py'])
+    subprocess.run(['python', 'NBAScrape.py'])
     #subprocess.run(['python3.10', 'CBBScrape.py'])
-    subprocess.run(['python3.10', 'Sort.py'])
+    subprocess.run(['python', 'Sort.py'])
     os.chdir("../")
 
     # Scrape Matchups
     os.chdir("./Scrapers/Books")
-    subprocess.run(['python3.10', 'daily_refresh.py'])
+    subprocess.run(['python', 'daily_refresh.py'])
 
     # Run Algorithms
     os.chdir("../../Data Collecting Src")
     with open("../Results/alg_results.txt", "w") as f:
-        subprocess.run(['python3.10', 'alg.py'], stdout=f)
+        subprocess.run(['python', 'alg.py'], stdout=f)
     with open("../Results/game_results.txt", "w") as f:
-        subprocess.run(['python3.10', 'game.py'], stdout=f)
+        subprocess.run(['python', 'game.py'], stdout=f)
     with open("../Results/SuccessData_results.txt", "w") as f:
-        subprocess.run(['python3.10', 'SuccessData.py'], stdout=f)
+        subprocess.run(['python', 'SuccessData.py'], stdout=f)
     os.chdir("../Results")
-    subprocess.run(['python3.10', 'summary.py'])
+    subprocess.run(['python', 'summary.py'])
 
     # Capture end time
     end_time = datetime.now()
