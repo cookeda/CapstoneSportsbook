aea3ebab2e48b569105ca414ff8b289084f11b4a
lknconnor <rkdconnor@gmail.com>
2024-04-26 10:31:31 -0400

    Automated Server Update

--- Update.py
+++ Update.py
@@ -6,7 +6,7 @@ import datetime
 start_time = datetime.datetime.now()
 
 # Scrape Matchups
-os.chdir('./Scrapers/Books')
+os.chdir('media/myfiles/CapstoneSportsbook/Scrapers/Books')
 subprocess.run(['python', 'daily_refresh.py'])
 
 # Run Algorithm
