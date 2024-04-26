2be972e80b2e955d67ffd7161995b76cfe87b908
lknconnor <rkdconnor@gmail.com>
2024-04-26 11:11:22 -0400

    Automated Server Update

--- Scrapers/Books/constant_refresh.py
+++ Scrapers/Books/constant_refresh.py
@@ -6,9 +6,9 @@ import time
 process1 = subprocess.Popen(["python", "script.py"], cwd='DK')
 process1.wait() 
 
-process2 = subprocess.Popen(["python", "script.py"], cwd='ESPN') 
+#process2 = subprocess.Popen(["python", "script.py"], cwd='ESPN') 
 process3 = subprocess.Popen(["python", "script.py"], cwd='Bovada')
-process2.wait() 
+#process2.wait() 
 process3.wait()
 
 process4 = subprocess.Popen(["python", "master_lite.py"])
