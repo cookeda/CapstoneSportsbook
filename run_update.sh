ad438979a66639af703e6de641c3711dc8938fdd
cookeda <cookeda@appstate.edu>
2024-04-27 11:46:49 -0400

    Server should be fixed

--- run_update.sh
+++ run_update.sh
@@ -6,8 +6,8 @@ source ~/.bashrc
 # Activate the Conda environment
 source /home/rkdconnor/anaconda3/bin/activate Capstone
 
-# Run the Python script
-python /media/myfiles/CapstoneSportsbook/Update.py
-
 # Execute the Git push script
 /media/myfiles/CapstoneSportsbook/git_push.sh
+
+# Run the Python script
+python /media/myfiles/CapstoneSportsbook/Update.py
