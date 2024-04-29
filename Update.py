4decce8d7f602da61698d2986f282ad5cc554e9c
lknconnor <rkdconnor@gmail.com>
2024-04-29 13:02:42 -0400

    Automated Server Update

--- Update.py
+++ Update.py
@@ -15,6 +15,7 @@ subprocess.run(['python', 'game.py'], stdout=open('../Results/game_results.txt',
 
 # Generate summary and merge results
 os.chdir('../Results/')
+time.sleep(3)
 subprocess.run(['python', 'script.py'])
 
 # Data Processing and Updates
