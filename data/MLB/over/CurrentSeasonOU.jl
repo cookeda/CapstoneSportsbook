4d2f1ebb4a7d2c6f1d1ce020953e2bfbeb7a386a
lknconnor <rkdconnor@gmail.com>
2024-04-28 21:07:14 -0400

    idk:

---
+++ data/MLB/over/CurrentSeasonOU.jl
@@ -0,0 +1,30 @@
+{"Team": "Baltimore", "Over %": "68.2%"} +1.7
+{"Team": "Cleveland", "Over %": "65.2%"} +0.8
+{"Team": "LA Dodgers", "Over %": "61.5%"} +0.7
+{"Team": "Milwaukee", "Over %": "58.3%"} +0.9
+{"Team": "Cincinnati", "Over %": "58.3%"} +0.8
+{"Team": "Tampa Bay", "Over %": "57.7%"} +0.2
+{"Team": "SF Giants", "Over %": "56.0%"} +1.0
+{"Team": "Atlanta", "Over %": "54.6%"} +1.0
+{"Team": "NY Mets", "Over %": "54.2%"} +0.7
+{"Team": "LA Angels", "Over %": "52.0%"} +0.3
+{"Team": "San Diego", "Over %": "51.9%"} +1.2
+{"Team": "Miami", "Over %": "50.0%"} +0.1
+{"Team": "Pittsburgh", "Over %": "50.0%"} +0.3
+{"Team": "Colorado", "Over %": "50.0%"} +0.3
+{"Team": "Arizona", "Over %": "48.0%"} +1.1
+{"Team": "Oakland", "Over %": "48.0%"} -0.9
+{"Team": "Detroit", "Over %": "47.8%"} -0.1
+{"Team": "Minnesota", "Over %": "47.8%"} +0.1
+{"Team": "Texas", "Over %": "46.2%"} +0.0
+{"Team": "Boston", "Over %": "45.8%"} -0.2
+{"Team": "Chi Sox", "Over %": "45.8%"} -0.3
+{"Team": "Chi Cubs", "Over %": "44.0%"} +1.3
+{"Team": "Philadelphia", "Over %": "40.0%"} +0.1
+{"Team": "Washington", "Over %": "39.1%"} -0.8
+{"Team": "Toronto", "Over %": "38.5%"} -0.3
+{"Team": "NY Yankees", "Over %": "36.0%"} -0.7
+{"Team": "St. Louis", "Over %": "34.8%"} -0.3
+{"Team": "Houston", "Over %": "33.3%"} +0.1
+{"Team": "Kansas City", "Over %": "32.0%"} -0.8
+{"Team": "Seattle", "Over %": "29.2%"} -0.9
