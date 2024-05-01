7936f861f5b67505bca3480db0cbf05d4c2c3a98
Devin Cooke <111797349+cookeda@users.noreply.github.com>
2024-05-01 10:51:19 -0400

    Update Update.py

--- Update.py
+++ Update.py
@@ -7,7 +7,7 @@ import time
 start_time = datetime.datetime.now()
 
 # Scrape Matchups
-os.chdir("./Scrapers/Books")
+os.chdir("/media/myfiles/CapstoneSportsbook/Scrapers/Books")
 subprocess.run(['python', 'daily_refresh.py'])
 
 # Run Algorithm
