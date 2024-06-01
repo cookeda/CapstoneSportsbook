f71247afb5079383c0cd6449dd6e82aa2ed4a553
cookeda <cookeda@appstate.edu>
2024-06-01 08:15:51 -0400

    6/1

--- data/MLB/cover/away/SortedawayCover.jl
+++ data/MLB/cover/away/SortedawayCover.jl
@@ -1,30 +1,30 @@
-{"Team": "Arizona", "Cover %": "46.4%"} +1.0
+{"Team": "Arizona", "Cover %": "48.3%"} +1.0
 {"Team": "Atlanta", "Cover %": "53.9%"} +0.0
 {"Team": "Baltimore", "Cover %": "58.3%"} +0.7
 {"Team": "Boston", "Cover %": "60.0%"} +2.1
 {"Team": "Chi Cubs", "Cover %": "58.1%"} +0.1
-{"Team": "Chi Sox", "Cover %": "30.8%"} -1.3
-{"Team": "Cincinnati", "Cover %": "64.0%"} +1.1
+{"Team": "Chi Sox", "Cover %": "29.6%"} -1.5
+{"Team": "Cincinnati", "Cover %": "65.4%"} +1.2
 {"Team": "Cleveland", "Cover %": "59.4%"} +1.6
-{"Team": "Colorado", "Cover %": "53.6%"} -0.4
-{"Team": "Detroit", "Cover %": "59.3%"} +1.6
+{"Team": "Colorado", "Cover %": "55.2%"} -0.3
+{"Team": "Detroit", "Cover %": "57.1%"} +1.4
 {"Team": "Houston", "Cover %": "42.3%"} -1.8
 {"Team": "Kansas City", "Cover %": "62.1%"} +1.1
-{"Team": "LA Angels", "Cover %": "67.9%"} +1.6
+{"Team": "LA Angels", "Cover %": "69.0%"} +1.6
 {"Team": "LA Dodgers", "Cover %": "55.6%"} +0.2
 {"Team": "Miami", "Cover %": "53.6%"} +0.6
 {"Team": "Milwaukee", "Cover %": "61.3%"} +1.6
 {"Team": "Minnesota", "Cover %": "53.6%"} +0.4
 {"Team": "NY Mets", "Cover %": "56.0%"} +0.5
-{"Team": "NY Yankees", "Cover %": "59.4%"} +1.3
-{"Team": "Oakland", "Cover %": "51.7%"} -0.1
+{"Team": "NY Yankees", "Cover %": "60.6%"} +1.3
+{"Team": "Oakland", "Cover %": "50.0%"} -0.1
 {"Team": "Philadelphia", "Cover %": "55.6%"} +0.5
-{"Team": "Pittsburgh", "Cover %": "62.1%"} +0.7
+{"Team": "Pittsburgh", "Cover %": "60.0%"} +0.6
 {"Team": "SF Giants", "Cover %": "51.7%"} -0.4
 {"Team": "San Diego", "Cover %": "74.1%"} +1.8
 {"Team": "Seattle", "Cover %": "44.8%"} -0.1
-{"Team": "St. Louis", "Cover %": "48.3%"} -0.1
-{"Team": "Tampa Bay", "Cover %": "54.6%"} -0.9
-{"Team": "Texas", "Cover %": "41.4%"} +0.0
+{"Team": "St. Louis", "Cover %": "46.7%"} -0.1
+{"Team": "Tampa Bay", "Cover %": "52.2%"} -0.8
+{"Team": "Texas", "Cover %": "40.0%"} -0.3
 {"Team": "Toronto", "Cover %": "54.8%"} -0.3
-{"Team": "Washington", "Cover %": "65.6%"} +1.3
+{"Team": "Washington", "Cover %": "63.6%"} +1.1
