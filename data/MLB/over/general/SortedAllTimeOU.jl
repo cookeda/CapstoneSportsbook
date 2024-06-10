ed83cee057aa7077aa9700df11387d7ce1cd847a
cookeda <cookeda@appstate.edu>
2024-06-10 12:59:44 -0400

    pre pull

--- data/MLB/over/general/SortedAllTimeOU.jl
+++ data/MLB/over/general/SortedAllTimeOU.jl
@@ -10,7 +10,7 @@
 {"Team": "Detroit", "Over %": "50.1%"} +0.5
 {"Team": "Houston", "Over %": "48.2%"} +0.4
 {"Team": "Kansas City", "Over %": "47.8%"} +0.3
-{"Team": "LA Angels", "Over %": "49.4%"} +0.5
+{"Team": "LA Angels", "Over %": "49.5%"} +0.5
 {"Team": "LA Dodgers", "Over %": "49.4%"} +0.4
 {"Team": "Miami", "Over %": "51.1%"} +0.5
 {"Team": "Milwaukee", "Over %": "49.6%"} +0.4
@@ -23,8 +23,8 @@
 {"Team": "SF Giants", "Over %": "48.8%"} +0.4
 {"Team": "San Diego", "Over %": "49.9%"} +0.5
 {"Team": "Seattle", "Over %": "49.1%"} +0.4
-{"Team": "St. Louis", "Over %": "49.0%"} +0.4
+{"Team": "St. Louis", "Over %": "48.9%"} +0.4
 {"Team": "Tampa Bay", "Over %": "48.7%"} +0.4
-{"Team": "Texas", "Over %": "48.5%"} +0.5
+{"Team": "Texas", "Over %": "48.6%"} +0.5
 {"Team": "Toronto", "Over %": "48.7%"} +0.4
 {"Team": "Washington", "Over %": "49.3%"} +0.5
